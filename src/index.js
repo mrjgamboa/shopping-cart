8cafa17b2e2e237f056587491a38d526d1a2fbf1
John Gamboa <gamboa.john@protonmail.com>
2022-03-01 13:49:02 +0800

    added basename in index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/shopping-cart">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
